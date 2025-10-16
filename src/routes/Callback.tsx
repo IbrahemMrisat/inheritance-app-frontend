52ca03ea5d9400b49494111eae696254709e6223
IbrahemMrisat <imrisat@gmail.com>
2025-10-16 15:22:15 +0300

    Update Callback.tsx

---
+++ src/routes/Callback.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react'
+import { handleCallback } from '../services/auth'
+import { useNavigate } from 'react-router-dom'
+
+export default function Callback() {
+  const navigate = useNavigate()
+  useEffect(() => { handleCallback(); navigate('/cases') }, [navigate])
+  return <div className="p-6">جارٍ التحقق من الجلسة...</div>
+}
